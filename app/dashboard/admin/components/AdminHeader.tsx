3cae4710d40bd86637850681d0302e5551869c7c
avanidubey17 <avanidubey1710@gmail.com>
2025-11-06 17:23:58 +0530

    Updated admin dashboard logic

--- app/dashboard/admin/components/AdminHeader.tsx
+++ app/dashboard/admin/components/AdminHeader.tsx
@@ -1,13 +1,17 @@
 "use client";
 import { Button } from "@/components/ui/button";
 
+import Link from "next/link";
 export function AdminHeader() {
   return (
     <header className="h-16 bg-white border-b flex items-center justify-between px-6">
       <h1 className="text-lg font-semibold text-gray-800">Admin Panel</h1>
-      <Button variant="outline" onClick={() => alert("Logout coming soon!")}>
+      <Link href="/">
+      <Button variant="outline">
         Logout
       </Button>
+      </Link>
+      
     </header>
   );
 }
